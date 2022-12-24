f77994330e6b31c790ba996b4b05f9086aa0aef5
BichuPg <115611237+BichuPg@users.noreply.github.com>
2022-12-24 19:22:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,9 +5,9 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.getenv("API_ID", "12124605"))
+    API_ID = int(os.getenv("API_ID", "12192489"))
     API_HASH = os.getenv("API_HASH", "5cf3577d85fd02286535ec2296934287")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5660564414:AAE2Hem9t5uouIXHlMMV6Rq-D8A8L1k5ku8")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "ed39e721f5d4fd6d3c05121c1661b8ea")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "mdiskcynitebot")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "String")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001558716347")) 
