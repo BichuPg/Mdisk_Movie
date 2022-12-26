e9d74a5345c27255967d8bdfab013b50387e5bd8
BichuPg <115611237+BichuPg@users.noreply.github.com>
2022-12-26 15:14:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/BichuPg/Mdisk_Movies.git /Mdisk_Movies
-cd /Mdisk_Movies
+git clone https://github.com/BichuPg/Mdisk_Movie.git /Mdisk_Movie
+cd /Mdisk_Movie
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
